e5cfc9489d2f66c32115447723fdfd83217cc482
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2022-03-05 13:51:24 +0000

    Add files via upload

--- apps/home.py
+++ apps/home.py
@@ -3,32 +3,17 @@ import leafmap.foliumap as leafmap
 
 
 def app():
-    st.title("Streamlit for Geospatial Applications")
+    st.title("Streamlit for Emergency General Surgery")
 
     st.markdown(
         """
-        This multi-page web app demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and open-source mapping libraries, 
-        such as [leafmap](https://leafmap.org), [geemap](https://geemap.org), [pydeck](https://deckgl.readthedocs.io), and [kepler.gl](https://docs.kepler.gl/docs/keplergl-jupyter).
-        This is an open-source project and you are very welcome to contribute your comments, questions, resources, and apps as [issues](https://github.com/giswqs/streamlit-geospatial/issues) or 
-        [pull requests](https://github.com/giswqs/streamlit-geospatial/pulls) to the [GitHub repository](https://github.com/giswqs/streamlit-geospatial).
+        This multi-page web app functions to experiment with methods to visualize and more deeply understand emergency care pathways 
+        interactive web apps created using [streamlit](https://streamlit.io) and open-source libraries, such as Plotly.
+        This is an open-source project and you are very welcome to contribute your comments, questions, resources.
 
         """
     )
 
     st.info("Click on the left sidebar menu to navigate to the different apps.")
 
-    st.subheader("Timelapse of Satellite Imagery")
-    st.markdown(
-        """
-        The following timelapse animations were created using the Timelapse web app. Click `Create Timelapse` on the left sidebar menu to create your own timelapse for any location around the globe.
-    """
-    )
 
-    row1_col1, row1_col2 = st.columns(2)
-    with row1_col1:
-        st.image("https://github.com/giswqs/data/raw/main/timelapse/spain.gif")
-        st.image("https://github.com/giswqs/data/raw/main/timelapse/las_vegas.gif")
-
-    with row1_col2:
-        st.image("https://github.com/giswqs/data/raw/main/timelapse/goes.gif")
-        st.image("https://github.com/giswqs/data/raw/main/timelapse/fire.gif")
