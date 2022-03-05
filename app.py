d7dcf7164107067eecd501cba60607a5b838e79f
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2022-03-05 13:08:32 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 from multiapp import MultiApp
 from apps import (
     basemaps,
-    census,
+    home,
+    housing,
+    plotly_maps,
     wms,
     xy,
 )
@@ -17,7 +19,13 @@ apps = MultiApp()
 apps.add_app("Home", home.app)
 apps.add_app("Create Timelapse", timelapse.app)
 apps.add_app("U.S. Real Estate Data", housing.app)
-apps.add_app("U.S. Census Data", census.app)
+apps.add_app("Search Basemaps", basemaps.app)
+apps.add_app("Pydeck Gallery", deck.app)
+apps.add_app("Heatmaps", heatmap.app)
+apps.add_app("Add Points from XY", xy.app)
+apps.add_app("Add Web Map Service (WMS)", wms.app)
+apps.add_app("Google Earth Engine (GEE)", gee.app)
+apps.add_app("Geolocation", device_loc.app)
 apps.add_app("Cesium 3D Map", cesium.app)
 apps.add_app("Plotly", plotly_maps.app)
 apps.add_app("Demo", demo.app)
