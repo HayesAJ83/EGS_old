fdd0c19b402e5998f43cd18b8dc22efe7b80d842
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2022-03-05 13:45:28 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 from multiapp import MultiApp
 from apps import (
-    basemaps,
     home,
-    housing,
     plotly_maps,
     wms,
     xy,
@@ -11,14 +9,10 @@ from apps import (
 
 st.set_page_config(layout="wide")
 
-
 apps = MultiApp()
 
 # Add all your application here
-
 apps.add_app("Home", home.app)
-apps.add_app("U.S. Real Estate Data", housing.app)
-apps.add_app("Search Basemaps", basemaps.app)
 apps.add_app("Add Points from XY", xy.app)
 apps.add_app("Add Web Map Service (WMS)", wms.app)
 apps.add_app("Plotly", plotly_maps.app)
